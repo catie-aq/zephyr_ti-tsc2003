afe7969124f1769c36ebf7c34913663874d3e3c8
andiaye33 <a.ndiaye@catie.fr>
2024-11-26 17:13:54 +0100

    Apply pre commit

--- samples/src/main.c
+++ samples/src/main.c
@@ -10,54 +10,53 @@
 
 LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
 
-
 #define REFRESH_RATE 100
 
-static const struct device *const touch_dev  = DEVICE_DT_GET(DT_NODELABEL(tsc20030));
+static const struct device *const touch_dev = DEVICE_DT_GET(DT_NODELABEL(tsc20030));
 
 static struct k_sem sync;
 
 static struct {
-    size_t x;
-    size_t y;
-    bool pressed;
+	size_t x;
+	size_t y;
+	bool pressed;
 } touch_point;
 
 static void touch_event_callback(struct input_event *evt)
 {
-    if (evt->code == INPUT_ABS_X) {
-        touch_point.x = evt->value;
-    }
-    if (evt->code == INPUT_ABS_Y) {
-        touch_point.y = evt->value;
-    }
-    if (evt->code == INPUT_BTN_TOUCH) {
-        touch_point.pressed = evt->value;
-    }
-    if (evt->sync) {
-        k_sem_give(&sync);
-    }
+	if (evt->code == INPUT_ABS_X) {
+		touch_point.x = evt->value;
+	}
+	if (evt->code == INPUT_ABS_Y) {
+		touch_point.y = evt->value;
+	}
+	if (evt->code == INPUT_BTN_TOUCH) {
+		touch_point.pressed = evt->value;
+	}
+	if (evt->sync) {
+		k_sem_give(&sync);
+	}
 }
 
 INPUT_CALLBACK_DEFINE(touch_dev, touch_event_callback);
 
 int main(void)
 {
-    LOG_INF("Touch sample for touchscreen: %s", touch_dev->name);
+	LOG_INF("Touch sample for touchscreen: %s", touch_dev->name);
 
-    if (!device_is_ready(touch_dev)) {
-        LOG_ERR("Device %s not found. Aborting sample.", touch_dev->name);
-        return 0;
-    }
+	if (!device_is_ready(touch_dev)) {
+		LOG_ERR("Device %s not found. Aborting sample.", touch_dev->name);
+		return 0;
+	}
 
-    k_sem_init(&sync, 0, 1);
+	k_sem_init(&sync, 0, 1);
 
-    while (1) {
-        k_msleep(REFRESH_RATE);
-        k_sem_take(&sync, K_FOREVER);
-        LOG_INF("TOUCH %s X, Y: (%d, %d)", touch_point.pressed ? "PRESS" : "RELEASE",
-            touch_point.x, touch_point.y);
-    }
+	while (1) {
+		k_msleep(REFRESH_RATE);
+		k_sem_take(&sync, K_FOREVER);
+		LOG_INF("TOUCH %s X, Y: (%d, %d)", touch_point.pressed ? "PRESS" : "RELEASE",
+			touch_point.x, touch_point.y);
+	}
 
-    return 0;
+	return 0;
 }
